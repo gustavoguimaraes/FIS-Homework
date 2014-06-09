eefd02f45dc2470d3c4ee43d01edeafc08ce9d7c
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2014-06-09 00:19:58 -0400

    Refator My_Min Exercise Getting Ready for Green_Grocer

--- my_min/my_min.rb
+++ my_min/my_min.rb
@@ -1,24 +1,7 @@
-#hash= {:key1=>23, :key2=>42, :key3=>16}
-
-# def my_min(hash)
-#   hash_values = []
-#   hash.collect do |key, value|
-#     hash_values << value
-#   end
-#   hash.key(hash_values.min)
-# end
-
-def my_min(hash)
-  min_array = []
-  min = hash.values[0]
-  i = 0
-  loop do
-
-    min_array << hash.values[i]
-    min = min_array[i] if min_array[i] < min
-    i += 1
-    break if i == hash.length
-
+def return_hash_key_with_lowest_value(hash)
+  hash_values_array = []
+  hash.collect do |key, value|
+    hash_values_array << value
   end
-  hash.key(min)
+  hash.key(hash_values_array.min)
 end
--- my_min/my_min_spec.rb
+++ my_min/my_min_spec.rb
@@ -1,16 +1,12 @@
 require_relative 'my_min.rb'
 
-describe '#my_min' do
-
-hash = {:key1=>23, :key2=>42, :key3=>16, :key4 => 20, key5: 200}
+describe '#return_hash_key_with_lowest_value' do
+  hash = {:key1=>23, :key2=>42, :key3=>16, :key4 => 20, key5: 200}
   it 'should not return nil' do
-    my_min(hash).should_not be_nil
+    expect(return_hash_key_with_lowest_value(hash)).not_to be_nil
   end
 
   it 'should return a key that points to the lowest value' do
-    my_min(hash).should == :key3
+    expect(return_hash_key_with_lowest_value(hash)).to eq(:key3)
   end
-
-
-
 end
