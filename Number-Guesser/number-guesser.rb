3f82cda2f9759bb7a3806cfc82bb332ae671817b
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2014-06-09 00:13:03 -0400

    Refactor Number game

--- Number-Guesser/number-guesser.rb
+++ Number-Guesser/number-guesser.rb
@@ -1,44 +1,45 @@
-
-prompt = puts "Guess a number between 1 and 100"
-user = gets.chomp.to_i
-computer =  1 + rand(100)
-
-def game(guess, user_guess)
-	until (guess == user_guess) do
-	    if  (user_guess > 0 && user_guess < guess)
-		    puts "It is more than that"
-		    puts " Please, guess again"
-            user_guess = gets.chomp.to_i
-		      
-	     elsif (user_guess > guess && user_guess < 101)
-	        puts "It is less than that" 
-	        puts " Please, guess again"
-            user_guess = gets.chomp.to_i
-	           
-	     else
-	        puts "Please type a numberls between 1 and 100"
-	        puts " Please, type again"
-            user_guess = gets.chomp.to_i
-        end
-    end
-
-
-   if user_guess == guess
-	  puts " You got it!"
-	  puts "Do you want to play again? Yes/No"
-	  ans = gets.chomp.capitalize
-	    if ans == "Yes"
-	      puts "Type another number between 1 and 100 to play again"
-	      user_guess = gets.chomp.to_i
-	      guess = 1 + rand(100)
-	      game(guess, user_guess)
-          elsif ans == "No"
-     	  puts "Nice playing with you, Bye!"
-          else
-     	  puts "You didn't say yes or no, so I am finishing the game anyway."
-	
-        end
-    end
+def prompt
+  puts "Guess a number between 1 and 100"
+  gets.chomp.to_i
 end
 
-game(computer, user)
+def please_type_again
+  puts " Please, guess again"
+  gets.chomp.to_i
+end
+
+def havent_guessed(user_guess, computer_number)
+  if (user_guess > 0 && user_guess < computer_number)
+   puts "It is more than that"
+   please_type_again
+  elsif (user_guess > computer_number && user_guess < 101)
+   puts "It is less than that"
+   please_type_again
+  else
+   puts "Please type a number between 1 and 100"
+   please_type_again
+  end
+end
+
+def congratulations
+  puts "You got it! Do you want to play again? Yes/No"
+  gets.chomp.downcase
+end
+
+def correct_guess(user_guess, computer_number)
+  return game if congratulations == "yes"
+  puts "Nice playing with you, Bye!"
+  false
+end
+
+def game
+  user_guess = prompt
+  computer_number = 1 + rand(100)
+  puts computer_number
+  while true
+    return correct_guess(user_guess, computer_number) if user_guess == computer_number
+    user_guess = havent_guessed(user_guess, computer_number)
+  end
+end
+
+game
